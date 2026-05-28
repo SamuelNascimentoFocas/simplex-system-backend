f9a417158c75d5ad1731d563289735e91601d48f
Samuel Nascimento Focas <samuelnascimentofocas@gmail.com>
2026-05-28 03:00:15 -0300

    initial backend setup

---
+++ start/routes.ts
@@ -0,0 +1,48 @@
+/*
+|--------------------------------------------------------------------------
+| Routes file
+|--------------------------------------------------------------------------
+|
+| The routes file is used for defining the HTTP routes.
+|
+*/
+
+import { middleware } from '#start/kernel'
+import router from '@adonisjs/core/services/router'
+import { controllers } from '#generated/controllers'
+
+router.get('/', () => {
+  return { message: 'API Simplex funcionando' }
+})
+
+router.post('/simplex/solve', async ({ request }) => {
+  const data = request.body()
+
+  console.log(data)
+
+  return {
+    received: data,
+    message: 'Dados recebidos com sucesso',
+  }
+})
+
+router
+  .group(() => {
+    router
+      .group(() => {
+        router.post('signup', [controllers.NewAccount, 'store'])
+        router.post('login', [controllers.AccessTokens, 'store'])
+      })
+      .prefix('auth')
+      .as('auth')
+
+    router
+      .group(() => {
+        router.get('profile', [controllers.Profile, 'show'])
+        router.post('logout', [controllers.AccessTokens, 'destroy'])
+      })
+      .prefix('account')
+      .as('profile')
+      .use(middleware.auth())
+  })
+  .prefix('/api/v1')
